9011cefbec46292842a45b5599aea84a3f09ac4a
Cadu <cadulbastos@gmail.com>
2024-08-20 23:34:07 -0300

    bugfix - while command sintaxe bug

--- monitoring-file-in-directory.sh
+++ monitoring-file-in-directory.sh
@@ -2,7 +2,7 @@
 
 #this script remains in loop until to detect algo.txt file in the directory 
 
-while[ -e "$algo.txt" ]; do 
+while [ -e "$algo.txt" ]; do 
     echo "We're waiting, "
     sleep 1
 done
