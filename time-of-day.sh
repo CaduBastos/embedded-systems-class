6e0afa3b1c644cb0804c96c7430f6e1a9774edf9
Cadu <cadulbastos@gmail.com>
2024-08-20 21:04:50 -0300

    bugfix

--- time-of-day.sh
+++ time-of-day.sh
@@ -1,11 +1,11 @@
-#! /bin/bash
+#!/bin/sh
 
 #this script ask the user if it is morning or afternoon and wishes him a good day or good afternoon
 
 echo "It is morning? Please answer yes or no"
 read answer
 
-if [$answer = "yes"]; then
+if [ $answer = "yes" ]; then
     echo "Good morning!"
 else 
     echo "Good afternoon!"
