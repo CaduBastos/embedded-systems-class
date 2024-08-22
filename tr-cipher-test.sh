2734bb4ace15465ac25dd334031be4dee04c45f6
Cadu <cadulbastos@gmail.com>
2024-08-22 20:18:09 -0300

    implemented monoalphabetic cipher feature

--- tr-cipher-test.sh
+++ tr-cipher-test.sh
@@ -7,7 +7,13 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-cat $0
+uppercase_original="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+uppercase_key="ZQ8WNBRLJCK4XY7TVSD2AH"
+
+original="abcdefghijklmnopqrstuvwxyz"
+key="pmzjtdhgrfycqvnbkwxlsu"
+
+cat $1 | tr ${uppercase_original} $uppercase_key | tr ${original} ${key} > file_encrypted.txt
 
 #cat ${FILE_NAME} | tr ${uppercase_original} ${uppercase_key} | tr ${original} ${key} > ${OUTPUT_FILE}
 
